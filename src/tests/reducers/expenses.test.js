200245fbb67626f59b4945a7b2f1faea82a04741
Narek Vardzelyan <narekcat@gmail.com>
2019-03-11 11:36:59 +0400

    Add functionality to fetch expenses from firebase.

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -75,3 +75,12 @@ test('should not edit an expense if not found', () => {
   const state = expensesReducer(expenses, action);
   expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+  const action = {
+    type: 'SET_EXPENSES',
+    expenses
+  };
+  const state = expensesReducer([], action);
+  expect(state).toEqual(expenses);
+});
